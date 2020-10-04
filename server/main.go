40c2265fc93c96ddaf3e698d0ab7215ce2b69fce
yamadashi <usaming27@gmail.com>
2020-10-04 21:57:01 +0900

    serveするディレクトリのパス指定を修正

--- server/main.go
+++ server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -11,10 +13,16 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintln(w, "pong pong")
+		fmt.Fprintln(w, "pong")
 	}).Methods("GET")
 	router.PathPrefix("/").Handler(
-		http.FileServer(http.Dir("public")))
+		http.FileServer(http.Dir(getProjectRootDir() + "public")))
 	fmt.Println("service start")
 	http.ListenAndServe(":8080", router)
+}
+
+func getProjectRootDir() string {
+	exe, _ := os.Executable()
+	projectRootDir := filepath.Dir(exe) + "../../"
+	return http.Dir(projectRootDir)
 }
